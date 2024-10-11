e27094a888bef549598972d4e497ea84257061cd
Tejas Giri <tejasgiri@ipgangram.com>
2024-10-11 12:19:18 +0530

    changed url

--- client/src/config/instance.js
+++ client/src/config/instance.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // Create an Axios instance with a base URL
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5000', // Replace with your API's base URL
+    baseURL: 'https://webdesk.onrender.com', // Replace with your API's base URL
 });
 
 // Export the Axios instance
